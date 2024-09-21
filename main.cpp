aecdbd266f226a66a0956995aaa138b68e758004
matteogirotti <matteogirotti10@gmail.com>
2024-09-21 21:31:10 +0200

    commit esercizio 2.2

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    float b, h, a;
+    printf("area triangolo:\nbase: ");
+    scanf("%f",&b);
+    printf("altezza: ");
+    scanf("%f",&h);
+    a = (b*h)/2;
+    printf("il risultato dell'area è: %f\n",a);
+}
